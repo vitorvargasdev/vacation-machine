16b7517e847ec729c3e9ec8356fa427494621e29
vitorvargasdev <instalando99@gmail.com>
2024-02-12 00:48:19 -0300

    first commit

---
+++ init.sh
@@ -0,0 +1,40 @@
+USER=$1
+PWD=$2
+
+pacman-key --init
+pacman -Sy --noconfirm reflector 
+
+reflector --latest 5 \
+  --protocol http,https \
+  --save "/etc/pacman.d/mirrorlist" \
+  --sort rate
+
+pacman -Syu --noconfirm base base-devel sudo zip unzip docker docker-compose go
+
+useradd -m -g users -G wheel $USER
+
+echo "%wheel ALL=(ALL:ALL) ALL" >> /etc/sudoers
+
+systemctl enable docker
+
+usermod -aG docker $USER
+
+echo -e "$PWD\n$PWD" | passwd $USER
+
+pacman -S --noconfirm git
+
+cp /etc/sudoers /etc/sudoers.bkp
+
+echo "$USER ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+
+cp /tmp/dev.zip /home/$USER/dev.zip
+chown $USER /home/$USER/dev.zip
+su $USER -c "unzip /home/$USER/dev.zip"
+rm /home/$USER/dev.zip
+
+su $USER -c "cd /home/$USER && mkdir -p /home/$USER/pkgs && cd /home/$USER/pkgs"
+su $USER -c "cd /home/$USER/pkgs && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si --noconfirm"
+su $USER -c "cd /home/$USER/pkgs && git clone https://aur.archlinux.org/go-task-bin.git && cd go-task-bin && makepkg -si --noconfirm"
+
+bash -c "cat /etc/sudoers.bkp > /etc/sudoers"
+rm /etc/sudoers.bkp
---
+++ machine.tf
@@ -0,0 +1,50 @@
+terraform {
+  required_providers {
+    linode = {
+      source  = "linode/linode"
+      version = "1.16.0"
+    }
+  }
+}
+
+provider "linode" {
+  token = var.linode_token
+}
+
+
+resource "linode_instance" "work_instance" {
+  label            = "work_instance"
+  image            = var.linode_image
+  region           = var.linode_region
+  type             = var.linode_type
+  authorized_users = var.linode_authorized_users
+  root_pass        = var.linode_root_pass
+  private_ip       = var.linode_private_ip
+
+  connection {
+    type = "ssh"
+    user = "root"
+    host = self.ip_address
+  }
+
+  provisioner "local-exec" {
+    command = "rm -rf dev.zip && zip -r dev.zip dev"
+  }
+
+  provisioner "file" {
+    source      = "dev.zip"
+    destination = "/tmp/dev.zip"
+  }
+
+  provisioner "file" {
+    source      = "init.sh"
+    destination = "/tmp/script.sh"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /tmp/script.sh",
+      "/tmp/script.sh ${var.linode_user} ${var.linode_pass}",
+    ]
+  }
+}
